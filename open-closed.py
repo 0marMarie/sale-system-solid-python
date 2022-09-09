c83ef6376f8e45f715e38eef05db083e3e0ff887
Omar Marie <omarhamed440@gmail.com>
2022-09-09 20:30:49 +0200

    🙌 Refactor payment method with open/closed principle

--- open-closed.py
+++ open-closed.py
@@ -3,8 +3,14 @@
 # Date       :  9 SEP 2022
 # Version    :  V 1.1
 
-## Problem : If we added a new payment method it will violate the open/closed principle
 
+## Design Problem:
+## If we added a new payment method we will need to modify the paymentMethod class
+
+## Solved Using:
+## Open Closed Principle
+
+from abc import ABC, abstractmethod
 
 class Order:
     
@@ -27,58 +33,56 @@ class Order:
             total += self.quantities[i] * self.prices[i]
         return total
 
-    def pay(self, payment_type, security_code):
-        """Paying the Order"""
-        if payment_type == "debit":
-            print("Processing debit payment type")
-            print(f"Verifying security code: {security_code}")
-            self.status = "paid"
-        elif payment_type == "credit":
-            print("Processing credit payment type")
-            print(f"Verifying security code: {security_code}")
-            self.status = "paid"
-        else:
-            raise Exception(f"Unknown payment type: {payment_type}")
-
 
 # Create a new Pay class to separate payment from Order class
-class paymentMethods:
+class PaymentMethods(ABC):
     """
-    Payment Methods class
+    Abstract class to solve the violation of open closed principle
 
     Benefits: After separation order has one responsipility and pay has one responsipility
               Increased Cohesion
     drawback: Introduced some coupling
     """
-    def debit_type(self, order, security_code):
-        """ 
-        This is the debit type method.
-
+    @abstractmethod
+    def pay(self, order, security_code):
+        """
+        Declaration of an abstract method to be initialized later
+        Benefits: We can extend code by adding more subclasses without modifying PaymentMethods.
+        
         @param order: this is the order object
         @param security_code: this is the security code for product
-        @return: None
         """
+        pass
+
+
+class DebitPayment(PaymentMethods):
+    def pay(self, order, security_code):
+        """Impementing the abstract method declared in PaymentMethod"""
         print("Processing debit payment type")
         print(f"Verifying security code: {security_code}")
         order.status = "paid"
 
-    def credit_type(self, order, security_code):
-        """ 
-        This is the credit type method.
-
-        @param order: this is the order object
-        @param security_code: this is the security code for product
-        @return: None
-        """
+class CreditPayment(PaymentMethods):
+    def pay(self, order, security_code):
+        """Impementing the abstract method declared in PaymentMethod"""
         print("Processing credit payment type")
         print(f"Verifying security code: {security_code}")
         order.status = "paid"
 
+class PaybalPayment(PaymentMethods):
+    def pay(self, order, security_code):
+        """Impementing the abstract method declared in PaymentMethod"""
+        print("Processing paybal payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
 
+
+## User code
 order = Order()
 order.add_item("Keyboard", 1, 50)
 order.add_item("SSD", 1, 150)
 order.add_item("USB cable", 2, 5)
 
 print(order.total_price())
-order.pay("debit", "0372846")
+processor = PaybalPayment()
+processor.pay(order, "0372846")
